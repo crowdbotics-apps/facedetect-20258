45d966f47c54caa6d622adc838b199d4bcaa637c
Crowdbotics <team@crowdbotics.com>
2020-09-14 11:27:14 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/facedetect_20258/MainActivity.java
@@ -0,0 +1,15 @@
+package com.facedetect_20258;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "facedetect_20258";
+    }
+}
